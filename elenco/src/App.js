bb870eec47594c936932215301444b1444ed1586
jameskenyon <james.kenyon08@gmail.com>
2020-07-12 17:22:12 +0100

    Initial setup.

---
+++ elenco/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import MainView from "./views/MainView";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <MainView />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ elenco/src/views/MainView.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// styles and components
+import "./MainView.css";
+
+export default class MainView extends React.Component {
+  render() {
+    return <div className="View MainView">Test</div>;
+  }
+}
